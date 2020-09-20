c2f7c5a7a4828d780399d6d6b5ad68122da68f17
ArsikUn <lazar10011@gmail.com>
2020-09-20 20:00:37 +0300

    пробую 2

--- Seniaaa/Seniaaa/Program.cs
+++ Seniaaa/Seniaaa/Program.cs
@@ -4,9 +4,18 @@ namespace Seniaaa
 {
     class Program
     {
-        static void Main(string[] args)
+        public static int Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("Введите конец диапазона от 1 до : ");
+            int end = int.Parse(Console.ReadLine());
+            int j = 1;
+            for (int i = 1; i <= end; i += j)
+            {
+                Console.Write("{0} ", i);
+                j = i - j;
+            }
+            Console.ReadKey();
+            return 0;
         }
     }
 }
