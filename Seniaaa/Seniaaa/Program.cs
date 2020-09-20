209ff81da4c1aa41fd5d0e6133743e2132302f48
ArsikUn <lazar10011@gmail.com>
2020-09-20 19:57:06 +0300

    пробую

---
+++ Seniaaa/Seniaaa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Seniaaa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
